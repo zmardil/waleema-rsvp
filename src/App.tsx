d5cbac5798ff53e62b37172cb99e2bb5f516e623
User <user@Keeneyes-MacBook-Pro.local>
2023-12-11 21:35:01 +0530

    test changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+import { firestore } from "./firebase-config";
+import { addDoc, collection } from "firebase/firestore";
+
+const collectionRef = collection(firestore, "invitees");
 
 function App() {
-  const [count, setCount] = useState(0)
+	const inviteeInitial = {
+		fullname: "",
+		type: "couple",
+	};
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const [invitee, setInvitee] = useState(inviteeInitial);
+
+	const saveInvitee = async () => {
+		await addDoc(collectionRef, invitee);
+		window.location.reload();
+	};
+
+	return (
+		<>
+			<p>{invitee.fullname}</p>
+			<input
+				type="text"
+				placeholder="Full Name"
+				onChange={(e) => setInvitee({ ...invitee, fullname: e.target.value })}
+			/>
+			<button onClick={saveInvitee}>Save</button>
+		</>
+	);
 }
 
-export default App
+export default App;
